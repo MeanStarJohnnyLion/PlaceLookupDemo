672fee420a071bc519fc3b4029131004ecd5ed4b
Johnny Morgan <johnnylion3@aol.com>
2022-11-17 10:04:56 -0500

    Initial Commit

---
+++ PlaceLookupDemo/PlaceLookupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookupDemoApp.swift
+//  PlaceLookupDemo
+//
+//  Created by Johnny Lion on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PlaceLookupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
